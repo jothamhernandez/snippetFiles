3ec955513e550678453023dfd8280a0f58da42d4
Jotham Hernandez <jotham_hernandez@hotmail.com>
2017-06-27 23:28:52 +0800

    Update bday.js

--- bday.js
+++ bday.js
@@ -1,3 +1,15 @@
+(function() {
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.type = 'text/javascript';
+    script.onload = function() {
+        var $ = window.jQuery;
+        // Use $ here...
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+})();
+
 $(document).ready(function(){
   alert("HELLO");
 });
